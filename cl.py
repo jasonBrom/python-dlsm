30dd5cf9e2dfd962a092132c3775d1cd37951ff2
jasonBrom <60287510+jasonBrom@users.noreply.github.com>
2021-01-30 03:33:33 +0800

    Update cl.py

--- cl.py
+++ cl.py
@@ -6,19 +6,21 @@ import re
 import os
 
 
-f = open("sm.txt")
+f = open("sm2.txt")
 line = f.readline()
 while line:
     #print line,
     yms=line
     print(line, end = '')
     line = f.readline()
-    yms = yms.replace("\n","")
-    sj = yms.split( )
-    wc = sj[3] + ":9999"
-    print("\033[7;32;41m%s\033[0m"%wc)
-    with open("wc.txt", "a") as write:
-        write.write(wc + "\n")
+    if "open" in yms:
+       yms = yms.replace("\n","")
+       sj = yms.split( )
+       wc = sj[3] + ":3128"
+       print("\033[7;32;41m%s\033[0m"%wc)
+       with open("wc1.txt", "a") as write:
+           write.write(wc + "\n")
+     
     
     time.sleep(0.00001)
 
