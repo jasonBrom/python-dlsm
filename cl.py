85b05005004f1fd06b996d9eb26fa767047baf8a
jasonBrom <60287510+jasonBrom@users.noreply.github.com>
2021-01-31 00:35:21 +0800

    Update cl.py

--- cl.py
+++ cl.py
@@ -11,17 +11,17 @@ line = f.readline()
 while line:
     #print line,
     yms=line
-    
+    print(line, end = '')
     line = f.readline()
     if "open" in yms:
        yms = yms.replace("\n","")
        sj = yms.split( )
        wc = sj[3] + ":3128"
-       //print("\033[7;32;41m%s\033[0m"%wc)
+       print("\033[7;32;41m%s\033[0m"%wc)
        with open("wc.txt", "a") as write:
            write.write(wc + "\n")
      
     
-    //time.sleep(0.00000001)
+    time.sleep(0.00000001)
 
 f.close()
